f48c20c2f00d28cee1e255b5a887bfd935231c80
gmgreer <beauski8@gmail.com>
2022-11-01 06:00:55 -0700

    first and last commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+       const offset = button.dataset.carouselButton === 'next' ? 1 : -1
+       const slides = button
+       .closest("[data-carousel]")
+       .querySelector("[data-slides]")
+
+       const activeSlide = slides.querySelector("[data-active]")
+       let newIndex = [...slides.children].indexOf(activeSlide) + offset
+       if (newIndex < 0) newIndex = slides.children.length - 1
+       if (newIndex >= slides.children.length) newIndex = 0
+
+       slides.children[newIndex].dataset.active = true
+       delete activeSlide.dataset.active
+    })
+})
+
+document.addEventListener('click', e => {
+    const isDropdownButton = e.target.matches("[data-dropdown-button]")
+    if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) {
+        return
+        
+    }
+   
+    let currentDropdown
+    if (isDropdownButton) {
+        currentDropdown = e.target.closest('[data-dropdown]')
+        currentDropdown.classList.toggle('active')
+    }
+
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        if (dropdown === currentDropdown) return
+        dropdown.classList.remove('active')
+    })
+})
+
